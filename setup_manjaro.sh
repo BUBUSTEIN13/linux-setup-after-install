e5ee2515606cc85adf81b178b3092bb0fa8de874
BUBUSTEIN <84980561+BUBUSTEIN13@users.noreply.github.com>
2024-07-26 20:26:38 +0300

    Update setup_manjaro.sh

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -6,13 +6,12 @@ PARU_DIR="/home/abc/paru"
 # Actualizare și upgrade sistem
 sudo pacman -Syyu --noconfirm
 
-
 # Schimbare oglinzi pacman pentru Germania, Austria și Olanda
 sudo pacman-mirrors --api --country Germany Netherlands Austria --protocol https
 sudo pacman-mirrors --fasttrack 5
 sudo pacman -Syyu --noconfirm
 # Instalare qt5-tools pentru utilitarul qtpaths
-sudo pacman -S --noconfirm qt5-tools
+sudo pacman -S --noconfirm qt5-tools qt6-tools
 
 # Instalare Paru în /home/abc/
 sudo pacman -S --needed --noconfirm base-devel
@@ -22,32 +21,31 @@ cd $PARU_DIR
 makepkg -si --noconfirm
 cd ~
 
-# Instalare Sweet Cursors de la URL specificat și setare ca default
-git clone https://aur.archlinux.org/sweet-theme-nova-git.git
-cd sweet-theme-nova-git
+git clone https://aur.archlinux.org/sweet-cursor-theme-git.git
+cd sweet-cursor-theme-git
 makepkg -si --noconfirm
 cd ~
 kcmshell6 kcm_cursortheme --args "Sweet-cursors"
 
-# Setare wallpaper Darkest Hour
-WALLPAPER_PATH="/usr/share/wallpapers/DarkestHour/"
+WALLPAPER_PATH="/usr/share/wallpapers/North/"
 plasma-apply-wallpaperimage $WALLPAPER_PATH
 
-# Instalare fonturi Microsoft, Font-Awesome și Noto Fonts
-paru -S --noconfirm ttf-ms-fonts ttf-font-awesome noto-fonts
+paru -S --noconfirm ttf-ms-fonts
 
-# Instalare driver Nvidia
-# sudo mhwd -a pci nonfree 0300
 sudo pacman -S --noconfirm --needed nvidia-settings
-# Instalare Librewolf și setare ca default
+
 paru -S --noconfirm librewolf-bin
 xdg-settings set default-web-browser librewolf.desktop
 
-paru -S --noconfirm jdk17-temurin jdk21-temurin jdk8-temurin gimp zip unzip tar
-
-# Instalare Discord, Vencord, Spotify, Spicetify, Steam, IntelliJ IDEA Community, Virtual Box, FreeTube, PrismLauncher, JDK 17 Temurin și JDK 21 Temurin, GIMP, ZIP
-paru -S --noconfirm discord spotify steam intellij-idea-community-edition virtualbox freetube-bin 
-paru -S --noconfirm prismlauncher 
+sudo pacman -S --noconfirm gimp zip unzip tar discord steam thunderbird goverlay unrar
+sudo pacman -S --noconfirm noto-fonts ttf-font-awesome noto-fonts-extra lutris
+sudo pacman -S --noconfirm virtualbox linux610-virtualbox-host-modules virtualbox-guest-iso virtualbox-guest-utils
+sudo pacman -S --noconfirm intellij-idea-community-edition
+sudo pacman -S firewalld
+sudo systemctl enable --now firewalld
+paru -S --noconfirm jdk17-temurin jdk21-temurin jdk8-temurin
+paru -S --noconfirm spotify freetube
+paru -S --noconfirm prismlauncher
 
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 
